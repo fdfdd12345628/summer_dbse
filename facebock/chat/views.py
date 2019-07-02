051bd689fa618e079b3f5a12ea9a8fdbe455c5c8
changchiajung <42213793+changchiajung@users.noreply.github.com>
2019-07-02 16:43:30 +0800

    page renew
    upgrade

--- facebock/chat/views.py
+++ facebock/chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.shortcuts import render
+from django.http import HttpResponse
 from django.utils.safestring import mark_safe
+from .models  import Notification
 import json
 
 
@@ -10,7 +11,21 @@ def index(request):
 
 
 def room(request, room_name):
-    return render(request, 'chat/room.html', {
-        'room_name_json': mark_safe(json.dumps(room_name)),
-    })
+    if request.method =="GET":
+        if request.user.is_authenticated:
+            userid = request.user.id
+        owner = request.user
+        All_Notification = Notification.objects.filter(to_user_id=owner)
+        print(All_Notification)
+        return render(request, 'chat/room.html', {
+            'room_name_json': mark_safe(json.dumps(room_name)),
+            'All_Notification': All_Notification,
+        })
+    if request.method == "POST":
+        seen_id = request.POST.getlist('seen_id[]', '')
+        Notification_Select_List = Notification.objects.filter(id__in=seen_id)
+        for ele in Notification_Select_List:
+            ele.seen = True
+            ele.save()
+        return HttpResponse(status=200)
 # Create your views here.
--- facebock/static/room.js
+++ facebock/static/room.js
@@ -4,28 +4,53 @@ $(document).ready(function(){
     });
 });
 function change_to_none_notification(){
+    // change notification icon
     $("#notification_icon").removeClass("Notification");
     $("#notification_icon").addClass("None_Notification");
 }
 function change_to_notification(){
+    // change notification icon
     $("#notification_icon").addClass("Notification");
     $("#notification_icon").removeClass("None_Notification");
 }
 function add_red_point(id){
+    // 新增通知數目
     if(parseInt($("#"+id).text())==0){
+    // 原數目為0不顯示小紅點 故新增時將其 icon,小紅點更改顯示
         change_to_notification();
         $("#"+id).css("display","inline-block")
     }
     $("#"+id).text((parseInt($("#"+id).text())+1).toString())
+    //增加數目
 }
 function seen_notification(id){
+    //查看通知時，將icon轉換 並將db中通知狀態改變
     change_to_none_notification();
+    // change icon
     $("#"+id).css("display","none");
     $("#"+id).text("0");
+    clean_seen();
+    //using template system data, so put in room.html
+    //-- ajax post to views.py and change state of notification's status (0 -> 1)
 }
+var notification_list_state=0
+//紀錄是否被點開
 $(function(){
     $("#notification_icon").on("click",function(){
-        console.log("click");
-        seen_notification("notification_redpoint");
+        if(notification_list_state ==0){
+            //icon 原本未被點開
+            seen_notification("notification_redpoint");
+            //查看通知
+            $("#notification_list_group").css("display","flex");
+            notification_list_state = 1;
+            //展開list 並更改var
+        }else{
+            //原本被打開
+            $("#notification_list_group").css("display","none");
+            notification_list_state = 0;
+            //收起list並更改var
+		    $("#notification_list_group").find(".list-group-item-primary").removeClass("list-group-item-primary")
+		    //當收起時改變未讀list的狀態為已讀
+        }
     });
 });
