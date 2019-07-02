b0971f5c6def20f3b556373d687f6962ef83c89b
fdfdd12345628 <fdfdd12345628@gmail.com>
2019-07-02 15:43:39 +0800

    notification database

--- facebock/chat/consumers.py
+++ facebock/chat/consumers.py
@@ -7,20 +7,23 @@ from django.contrib.auth.models import User
 from .models import Notification, Group, Message
 import datetime
 
+
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
         if self.scope["user"].is_anonymous:
-            self.user_name="anonymous"
+            self.user_name = "anonymous"
         else:
             self.user_name = self.scope["user"].username
+            self.user = self.scope['user']
+            print(self.user)
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name,
         )
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.user_name,
             self.channel_name,
         )
@@ -32,7 +35,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name,
+        )
+        await self.channel_layer.group_discard(
             self.user_name,
+            self.channel_name,
         )
 
     # Receive message from WebSocket
@@ -42,80 +48,101 @@ class ChatConsumer(AsyncWebsocketConsumer):
         cate = text_data_json['type']
         user_name = text_data_json['user_name']
         # Send message to room group
-        await self.channel_layer.group_send(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message,
-                'cate':cate,
-            }
-        )
-        async_to_sync(self.channel_layer.group_send)(
-            user_name,
-            {
-                'type': 'notification_message',
-                'message': message,
-                'cate':cate,
-            }
-        )
+        if cate == 'chat':
+            await self.channel_layer.group_send(
+                self.room_group_name,
+                {
+                    'type': 'chat_message',
+                    'message': message,
+                    'cate': cate,
+                }
+            )
+        elif cate == 'notification':
+            await self.channel_layer.group_send(
+                user_name,
+                {
+                    'type': 'notification_message',
+                    'message': message,
+                    'cate': cate,
+                }
+            )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
         cate = event['cate']
         # Send message to WebSocket
-        if cate == "chat":
-            self.send(text_data=json.dumps({
-                'message': message,
-                'cate':cate,
-            }))
 
-    def notification_message(self, event):
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'cate': cate,
+        }))
+        '''await self.put_message(text=message,
+                               group=)
+        '''
+
+    async def notification_message(self, event):
         message = event['message']
         cate = event['cate']
         # Send message to WebSocket
         if cate == "notification":
-            self.send(text_data=json.dumps({
+            await self.send(text_data=json.dumps({
                 'message': message,
-                'cate':cate,
+                'cate': cate,
             }))
+            await self.put_notification(text=message,
+                                        to_user=self.user)
+
 
     @database_sync_to_async
-    def get_notification(self):
+    def get_notification(self, notification_id):
+        return Notification.objects.get(pk=notification_id)
         pass
+
 
     @database_sync_to_async
     def get_user(self, user_id):
         return User.objects.get(pk=user_id)
 
+
     @database_sync_to_async
     def put_notification(self, text, to_user):
-        notification=Notification(from_user=self.user,
-                                  to_user=to_user,
-                                  content=text,
-                                  date=datetime.datetime.now(),
-                                  seen=False,
-                                  )
+        notification = Notification(from_user=self.user,
+                                    to_user=to_user,
+                                    content=text,
+                                    date=datetime.datetime.now(),
+                                    seen=False,
+                                    )
         notification.save()
         pass
 
     @database_sync_to_async
     def get_message(self):
-
         pass
 
     @database_sync_to_async
-    def put_message(self, text, user=None, group=None):
-        if user is not None:
-            message=Message(from_user=self.user,
-                            content=text,
-                            date=datetime.datetime.now(),
-                            )
-        elif group is not None:
-            pass
+    def put_message(self, text, group=None):
+        if group is not None:
+            message = Message(from_user=self.user,
+                              content=text,
+                              date=datetime.datetime.now(),
+                              to_group=group, )
+
+            message.save()
         else:
             return
+
+    @database_sync_to_async
+    def create_group(self, user_1, user_2=None):
+        group = Group()
+        group.user.add(user_1)
+        group.user.add(user_2)
         pass
 
-    def create_group(self):
+    @database_sync_to_async
+    def group_add_user(self, group, user):
+        group.user.add(user)
+        group.save()
+
+    def get_group(self, group_id, ):
         pass
--- facebock/chat/models.py
+++ facebock/chat/models.py
@@ -6,7 +6,7 @@ import datetime
 # Create your models here.
 
 class Group(models.Model):
-    user = models.ManyToManyField(User)
+    user = models.ManyToManyField(User, related_name='group_user')
 
 
 class Notification(models.Model):
@@ -21,5 +21,5 @@ class Message(models.Model):
     from_user = models.ForeignKey(User, on_delete=None, related_name='message_from_user')
     content = models.CharField(max_length=1000)
     date = models.DateTimeField()
-    to_group = models.ForeignKey(Group, on_delete=None, related_name='to_group')
-    seen = models.ManyToManyField(User, related_name='seen', null=True)
+    to_group = models.ForeignKey(Group, on_delete=None, related_name='message_to_group')
+    seen = models.ManyToManyField(User, related_name='message_seen', null=True)
