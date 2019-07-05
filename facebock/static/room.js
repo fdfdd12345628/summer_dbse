b97a2a1f084c425978cf050fbcb927aad059a227
changchiajung <42213793+changchiajung@users.noreply.github.com>
2019-07-05 10:50:53 +0800

    Comment
    加入註解
    WIP: 初步webauthn 設定
    TO DO:  webauthn.py中verify進行到step14會因為fmt = tpm error

--- facebock/static/room.js
+++ facebock/static/room.js
@@ -1,9 +1,3 @@
-$(document).ready(function(){
-    $('#action_menu_btn').click(function(){
-	    $('.action_menu').toggle();
-    });
-
-});
 function getCookie(name) {
     var cookieValue = null;
     if (document.cookie && document.cookie != '') {
@@ -20,8 +14,9 @@ function getCookie(name) {
     return cookieValue;
     }
     var csrftoken = getCookie('csrftoken');
-
-
+/******
+for csrf token
+*******/
 function change_to_none_notification(){
     // change notification icon
     $("#notification_icon").removeClass("Notification");
@@ -32,6 +27,13 @@ function change_to_notification(){
     $("#notification_icon").addClass("Notification");
     $("#notification_icon").removeClass("None_Notification");
 }
+/******
+change notification icon image
+Usage:
+    Change icon's id
+    Change icon's background-image in static/room.css
+******/
+
 function add_red_point(id){
     // 新增通知數目
     if(parseInt($("#"+id).text())==0){
@@ -42,6 +44,11 @@ function add_red_point(id){
     $("#"+id).text((parseInt($("#"+id).text())+1).toString())
     //增加數目
 }
+/******
+Usage:
+    put <span class="redpoint" id="notification_redpoint">0</span> in notification icon's div
+******/
+
 function seen_notification(id){
     //查看通知時，將icon轉換 並將db中通知狀態改變
     change_to_none_notification();
@@ -49,11 +56,38 @@ function seen_notification(id){
     $("#"+id).css("display","none");
     $("#"+id).text("0");
     clean_seen();
-    //using template system data, so put in room.html
     //-- ajax post to views.py and change state of notification's status (0 -> 1)
 }
+/******
+    id => notification icon's id
+******/
+function clean_seen(){
+	var ids = $('.list-group-item-primary').map(function() {
+  		return $(this).attr('id').split("notification_")[1];
+	}).toArray();
+	$.ajax({
+   		type: 'POST',
+   		url: '',
+   		data: {
+   			type: "Clean_Seen",
+   			seen_id : ids,
+       		'csrfmiddlewaretoken':  csrftoken ,
+       	},
+       	dataType: 'json',
+       	success: function(content){
+			console.log("success");
+       	},
+    })
+}
+/******
+	將database中標示未讀者改為已讀
+	ids: parse class中有.list-group-item-primary (未讀) 記錄成array
+******/
+
+
+
 var notification_list_state=0
-//紀錄是否被點開
+//紀錄是否被點開 為了改變icon狀態
 $(function(){
     $("#notification_icon").on("click",function(){
         if(notification_list_state ==0){
@@ -73,6 +107,8 @@ $(function(){
         }
     });
 });
+
+
 $(function(){
     $(".other_user").click(function(){
         console.log(this.id.split("user_")[1])
@@ -94,13 +130,36 @@ $(function(){
        		},
         })
     })
+    /******
+    { one to one chat }
+    點擊聊天室 USER 時觸發
+    first: Post to query if group is exist (if not, create t) : return group id
+    Second: check if room already existed
+           if not, append chat room in "ChatRoom_Position"
+    Usage:
+        聊天室中 user 的 id 為 user_{{ model中 user 的id}}
+        改變前端
+           將append內容改變
+           id: display_room_{{ model中 group 的id }}
+           後方span處 填入 {{ model中 group 的display_name }}
+    ******/
     $(".exist_room").click(function(){
         console.log(this.id)
         if($("#display_" + this.id ).length == 0) {
             $("#ChatRoom_Position").append('<div class="col-md-6 col-xl-6 chat" id="display_'+this.id+'"><div class="card"><div class="card-header msg_head"><div class="d-flex bd-highlight"><div class="img_cont"><img src="https://image.flaticon.com/icons/svg/784/784662.svg" class="rounded-circle user_img"><span class="online_icon"></span></div><div class="user_info"><span>'+$("#"+this.id).find(".user_info > span").text()+'</span></div></div></div><div class="card-body msg_card_body"></div><div class="card-footer"><div class="input-group"><div class="input-group-append"><span class="input-group-text attach_btn"><i class="fas fa-paperclip"></i></span></div><textarea name="" class="form-control type_msg" placeholder="Type your message..."></textarea><div class="input-group-append"><span class="input-group-text send_btn"><i class="fas fa-location-arrow"></i></span></div></div></div></div></div>')
         }
     })
-
+    /******
+    { one to one chat }
+    { 等 one to many 可創建也同樣方式}
+    點擊聊天室 Group 時觸發
+    Usage:
+        聊天室中 group 的 id 為 room_{{ model 中 group的id}}
+        改變前端
+           將append內容改變
+           id: display_{{this.id}}
+           後方span處 填入 {{ this中找到display_name }}
+    ******/
 
     $(document).on("keypress",".type_msg",function (e) {
         if(e.which == 13) {
@@ -116,5 +175,14 @@ $(function(){
             setTimeout(function(){ $this.val("") }, 3);
         }
     });
+    /******
+    當在聊天室中輸入完成 並按下enter 即可傳送
+    使用chatsocket (已在room.html中定義)
+    Usage:
+        message: 欲傳送訊息
+        type: 判斷ws是通知或是聊天訊息
+        groupname: 判斷ws傳送對象 ( "group" )
+        {因 通知對人 聊天對聊天室}
+    ******/
 })
 
